461214bd2642a1e33800ed3eb971b522f79858dd
resident5 <Valencio_Williams@Bloomfield.edu>
2017-12-30 03:11:59 -0500

    Committing folder

---
+++ Logic Engines/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public MenuWheel wheel;
+
+	void Start ()
+	{
+		wheel = FindObjectOfType<MenuWheel> ();
+	}
+
+	void Update ()
+	{
+		Inputs ();
+	}
+
+	void Inputs ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			wheel.TurnUp ();
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow))
+		{
+			wheel.TurnDown ();
+		}
+			
+	}
+
+}
---
+++ Logic Engines/Assets/Scripts/MenuWheel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuWheel : MonoBehaviour
+{
+	public int buttonsNum;
+	public int angle;
+
+	public float lerpAngle;
+
+	public bool turnUp;
+	public bool turnDown;
+	public float turnSpeed;
+
+	Vector3 curAngle;
+	Vector3 nextAngle;
+
+	void Start ()
+	{
+		CalculateAngle ();
+
+		curAngle = new Vector3 (0, 0, 0);
+		nextAngle = new Vector3 (0, 0, curAngle.z + angle);
+	}
+
+	void FixedUpdate ()
+	{
+		if (turnUp)
+		{
+			transform.eulerAngles = Vector3.Lerp (transform.eulerAngles, nextAngle, turnSpeed);
+
+			if (Vector3.Distance (transform.eulerAngles, nextAngle) <= 0.1)
+			{
+				Debug.Log ("Euler: " +  transform.eulerAngles);
+				Debug.Log ("Next Angle" + nextAngle);
+
+				transform.eulerAngles = nextAngle;
+
+				curAngle = nextAngle;
+
+				nextAngle.z = curAngle.z + angle;
+
+				turnUp = false;
+			}
+		}
+		
+	}
+
+	public void TurnUp ()
+	{
+		turnUp = true;
+	}
+
+	public void TurnDown ()
+	{
+		turnDown = true;
+	}
+
+	void CalculateAngle ()
+	{
+		buttonsNum = transform.childCount;
+		angle = 360 / buttonsNum;
+	}
+
+}
